926e48a977e5ecbcb409cbe422567b17fb7dfc20
berrylradianh <berrylhamesha@gmail.com>
2025-09-27 18:19:11 +0700

    try 1

--- resources/views/landing/layout/app.blade.php
+++ resources/views/landing/layout/app.blade.php
@@ -1,14 +1,17 @@
 <!DOCTYPE html>
 <html lang="id">
+
 <head>
+    <base href="{{ url('/') }}/">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('page-title') - DesaInovatif</title>
-    <link rel="stylesheet" href="{{ asset('css/landing.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/all.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/font.css') }}">
+    <link rel="stylesheet" href="/css/landing.css">
+    <link rel="stylesheet" href="/css/all.min.css">
+    <link rel="stylesheet" href="/css/font.css">
 </head>
+
 <body>
     <!-- Header -->
     <header class="header">
@@ -24,4 +27,5 @@
     @include('landing.partial.footer')
     <script src="{{ asset('js/landing.js') }}"></script>
 </body>
+
 </html>
